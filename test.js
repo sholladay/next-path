17d9dfdf1fe64fad73b43505c4f92ba8aed337f0
Seth Holladay <me@seth-holladay.com>
2017-02-05 04:03:39 -0500

    Add more tests.

--- test.js
+++ test.js
@@ -2,12 +2,36 @@ import test from 'ava';
 import nextPath from '.';
 
 test('nextPath() basics', (t) => {
+    t.throws(
+        () => {
+            nextPath();
+        },
+        TypeError
+    );
+
     t.is(typeof nextPath('a', 'b'), 'string');
-    t.is(nextPath('/a/b/c/d', '/a'), '/a/b/c');
+    t.is(nextPath('/a', '/a/b/c'), '/a/b');
 });
 
-test('handles relative paths', (t) => {
+test('handle relative paths', (t) => {
+    t.is(nextPath('', ''), '.');
+    t.is(nextPath('.', '.'), '.');
+    t.is(nextPath('.', './'), '.');
+    t.is(nextPath('./', '.'), './');
+    t.is(nextPath('./', './'), './');
+
+    t.is(nextPath('.', 'a/b/c'), 'a');
+    t.is(nextPath('./', 'a/b/c'), 'a');
+    t.is(nextPath('.', './a/b/c'), 'a');
+
     t.is(nextPath('a', 'a/b/c'), 'a/b');
+    t.is(nextPath('a', './a/b/c'), 'a/b');
+    t.is(nextPath('./a', 'a/b/c'), 'a/b');
+    t.is(nextPath('./a', './a/b/c'), 'a/b');
+});
+
+test('can go up', (t) => {
+    t.is(nextPath('/a/b/c/d', '.'), '/a/b/c');
     t.is(nextPath('/a/b/c/d', '../a'), '/a/b/c');
-    t.is(nextPath('../../a', '/foo'), '../..');
+    t.is(nextPath('/a/b/c/d', '/a'), '/a/b/c');
 });
